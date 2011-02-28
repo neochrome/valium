0eac773e6b0d2dba98e1dd48254b98c97c150e5e
neochrome <johan@stenqvist.net>
2011-02-28 17:41:33 +0100

    more specs added

--- jquery.valium.spec.js
+++ jquery.valium.spec.js
@@ -20,6 +20,49 @@ describe('jQuery Valium', function () {
 		expect(form.valium('isValid')).toBe(true);
 	});
 
+	it('should validate with no rules specified', function(){
+		var form = build(function(form){
+			form.text('field1');
+		});
+		form.valium();
+
+		expect(form.valium('isValid')).toBe(true);
+		expect(form.find('#field1').valium('isValid')).toBe(true);
+	});
+
+	it('form should be invalid when at least one field is invalid', function(){
+		var form = build(function(form){
+			form.text('field1');
+			form.text('field2');
+		});
+		form.valium({field1:{required:{message:'required'}}});
+
+		expect(form.find('#field1').valium('isValid')).toBe(false);
+		expect(form.find('#field2').valium('isValid')).toBe(true);
+		expect(form.valium('isValid')).toBe(false);
+	});
+	
+	it('form should validate when all fields are valid', function(){
+		var form = build(function(form){
+			form.text('field1');
+			form.text('field2');
+			form.text('field3');
+		});
+		form.valium({
+			field1:{required:{message:'required'}},
+			field2:{required:{message:'required'}},
+			field3:{required:{message:'required'}},
+		});
+		form.find('#field1').val('something');
+		form.find('#field2').val('something');
+		form.find('#field3').val('something');
+
+
+		expect(form.find('#field1').valium('isValid')).toBe(true);
+		expect(form.find('#field2').valium('isValid')).toBe(true);
+		expect(form.find('#field3').valium('isValid')).toBe(true);
+		expect(form.valium('isValid')).toBe(true);
+	});
 
 	function build(fields){
 		var form = new Form();
