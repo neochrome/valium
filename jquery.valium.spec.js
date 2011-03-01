2a897cb68364a1b5abc0b4c33a4c299576431165
neochrome <johan@stenqvist.net>
2011-03-01 07:48:14 +0100

    moved form builder to separate file

--- jquery.valium.spec.js
+++ jquery.valium.spec.js
@@ -95,39 +95,4 @@ describe('jQuery Valium', function () {
 		expect(form.find(':input[name=field]').valium('isValid')).toBe(false);
 	});
 
-
-	function build(fields){
-		var form = new Form();
-		(fields || function(){})(form);
-		$('#html-hook').empty();
-		form.html.appendTo($('#html-hook'));
-		return form.html;
-	};
-
-	var Form = function(){
-		this.html = $('<form id="form"></form>');
-	};
-	Form.prototype = {
-		text: function(name){
-			$('<input type="text" id="' + name + '" name="' + name + '" />').appendTo(this.html);
-		},
-		select: function(name, options){
-			var select = $('<select id="' + name + '" name="' + name + '"></select>');
-			$.each(options, function(value, text){
-				select.append($('<option value="' + value + '">' + text + '</option>'));
-			});
-			select.appendTo(this.html);
-		},
-		check: function(name){
-			$('<input type="check" id="' + name + '" name="' + name + '" />').appendTo(this.html);
-		},
-		radio: function(name, options){
-			var form = this.html;
-			$.each(options, function(value, text){
-				form.append($('<input type="radio" id="' + name + '-' + value + '" name="' + name +'" value="' + value + '"/>'));
-				form.append($('<label for="' + name + '-' + value + '">' + text + '</label>'));
-			});
-		}
-	};
-
 });
